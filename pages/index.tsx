d3d0d77c9d99592b374d95a8c9bfe4bdaf80e56f
Alexander Barlow <alex.barlow@rocketmail.com>
2023-12-21 10:58:40 -0500

    fixed styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Spline from "@splinetool/react-spline";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [splineLoaded, setSplineLoaded] = useState(false)
+
+   useEffect(() => {
+     // Simulate loading delay
+     const simulateLoading = () => {
+       setSplineLoaded(true);
+     };
+
+     simulateLoading();
+   }, []); //
+
   return (
-    <div className="flex flex-col items-center lg:h-screen h-dvh bg-violet-950">
+    <div className="flex flex-col items-center h-fit lg:h-screen bg-violet-950">
       <div className="p-4 border m-5 w-3/4">
         {/* Content for the top box */}
         <h1 className="text-white text-center">The Web Developer Blog</h1>
@@ -27,8 +40,20 @@ export default function Home() {
           {/* Repeat your content structure as needed */}
         </div>
         <div className="w-full sm:w-1/2 p-4 border m-3 lg:mt-4 sm:mt-4">
-          {/* Adjust the style of the Spline container */}
-          <Spline scene="https://prod.spline.design/zNglQs9EoQ2ilpFK/scene.splinecode" />
+          {/* Conditionally render loading scene or Spline component */}
+          {splineLoaded ? (
+            <div className="w-full h-unit-9xl">
+              {/* Adjust the height as needed */}
+              <Spline
+                scene="https://prod.spline.design/zNglQs9EoQ2ilpFK/scene.splinecode"
+              />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center h-unit-9xl">
+              <p className="text-white">Loading Spline scene...</p>
+              {/* You can also add a spinner or other loading elements here */}
+            </div>
+          )}
         </div>
       </div>
     </div>
